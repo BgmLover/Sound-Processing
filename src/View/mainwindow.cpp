d7dfc29f4c07b0a8f0f3193f967a5e9917ea3ff7
BgmLover <787782453@qq.com>
2017-07-10 20:22:21 +0800

    Add files via upload

---
+++ src/View/mainwindow.cpp
@@ -0,0 +1,19 @@
+#include "src/View/mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+
+}
