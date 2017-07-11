2995e20058de7d992b0ddc5c2ed0a7489684db24
BgmLover <787782453@qq.com>
2017-07-11 23:08:19 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,22 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    shared_ptr<Model> pModel(new Model());
+    shared_ptr<ViewModel> pViewModel(new ViewModel());
+    shared_ptr<MainWindow> pMainWindows(new MainWindow);
+    pViewModel->setmodel(pModel);
+
+    //bind
+    pMainWindows->setplaylist(pViewModel->getplayList());
+    pMainWindows->bindplaylist();
+    pMainWindows->setaddMusicToListCommand(pViewModel->getAddMusicToListCommand());
+
+    pModel->addObserver(pViewModel);
+    pViewModel->addObserver(pMainWindows);
+
+    pMainWindows->show();
 
     return a.exec();
+
+
 }
