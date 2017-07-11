64cd160ff7580f527f051549a4668580e08a89af
seeForYou <641119121@qq.com>
2017-07-11 23:27:35 +0800

    Update Params.h

--- src/Common/Params.h
+++ src/Common/Params.h
@@ -4,28 +4,28 @@
 #include<QString>
 #include<vector>
 #include<memory>
-#include<string>
+
 using namespace std;
 
 class Params
 {
 public:
 
-   int getvolume(){return volume;}
-   void setvolume(const int &volume){this->volume=volume;}
-   qint64 getposition(){return position;}
-   void setposition(const qint64 &position){this->position=position;}
-   int getindex(){return currentindex;}
-   void setcurrentindex(const int &index){this->currentindex=index;}
-   QString getpath(){return path;}
-   void setstring(const QString &string){this->path=string;}
-   //int gettype(){return type;}
-  // void settype(const int &type){this->type=type;}
-private:
-    int volume=100;
-    qint64 position=0;  //milliseconds
-    int currentindex;   //列表中的顺序
-    QString path;
-};
+    //   int getvolume(){return volume;}
+    //   void setvolume(const int &volume){this->volume=volume;}
+    //   qint64 getposition(){return position;}
+    //   void setposition(const qint64 &position){this->position=position;}
+       int getindex(){return currentindex;}
+       void setcurrentindex(const int &index){this->currentindex=index;}
+       vector<QString>getpath(){return path;}
+       void setstring(const vector<QString> &string){path=string;}
+       //int gettype(){return type;}
+      // void settype(const int &type){this->type=type;}
+
+
+    private:
+        int currentindex;   //列表中的顺序
+       vector<QString> path;
+    };
 
 #endif
