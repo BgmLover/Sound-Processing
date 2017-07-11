795df95f6226badc6a531dc1f6fe43ce4100775a
seeForYou <641119121@qq.com>
2017-07-11 23:17:53 +0800

    Update AddMuiscToListCommand.h

--- src/ViewModel/Commands/AddMuiscToListCommand.h
+++ src/ViewModel/Commands/AddMuiscToListCommand.h
@@ -1,16 +1,18 @@
 #ifndef ADDMUSICCOMMAND
 #define ADDMUSICCOMMAND
 #include"src/Common/BaseCommand.h"
-
-class AddMusicToListCommand: public BaseCommand
+#include <QFileDialog>
+#include <QFileInfo>
+class ViewModel;
+class AddToListCommand: public BaseCommand
 {
 public:
-    AddMusicToListCommand(const shared_ptr<Model> &pModel) : BaseCommand(pModel) {}
-    void exec(){
-        QString path=params.getpath();
-        this->pModel->addPath(path);
+
+    virtual void exec()
+    {
+        pViewModel->AddMusicToList(params.getpath());
+
     }
 
 };
-
 #endif
