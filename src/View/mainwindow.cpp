cfc4ab0822e9a8b786d685592c99a068af8596a0
seeForYou <641119121@qq.com>
2017-07-11 23:22:55 +0800

    Update mainwindow.cpp

--- src/View/mainwindow.cpp
+++ src/View/mainwindow.cpp
@@ -1,10 +1,16 @@
 #include "src/View/mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QFileDialog>
+#include <QFileInfo>
+#include<QDebug>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    playList = shared_ptr<QMediaPlaylist>(new QMediaPlaylist);
+    player = shared_ptr<QMediaPlayer>(new QMediaPlayer);
+//	player->setPlaylist(playList.get());
+
     ui->setupUi(this);
 }
 
@@ -13,7 +19,43 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_pushButton_clicked()
+void MainWindow::update(Notify noti)
 {
-
+    switch(noti.gettype())
+    {
+    case Notifys::AddToListFinished :
+         {
+             qDebug() << "Add music successfully!";
+             break;
+         }
+    }
 }
+
+
+void MainWindow::on_PlayButton_clicked()
+{
+    player->setPlaylist(playList.get());
+    qDebug() << playList->isEmpty()<<"   "<< playList->mediaCount();
+
+       player->play();
+}
+
+
+
+void MainWindow::on_AddMuiscButton_clicked()
+{
+    QString initialName=QDir::homePath();
+    QStringList pathList=QFileDialog::getOpenFileNames(this, tr("选择文件"), initialName, tr("*.wav *mp3"));
+    vector<QString>temp;
+    for(int i=0; i<pathList.size(); ++i)
+    {
+        QString path=QDir::toNativeSeparators(pathList.at(i));
+        temp.push_back(path);
+     }
+    Params param;
+
+    param.setstring(temp);
+    addMusicToListCommand->setParams(param);
+    addMusicToListCommand->exec();
+}
+
