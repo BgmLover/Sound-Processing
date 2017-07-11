2ca9a795de5aae15838f13b97cb2f0a0eaab034a
seeForYou <641119121@qq.com>
2017-07-11 23:23:15 +0800

    Update mainwindow.h

--- src/View/mainwindow.h
+++ src/View/mainwindow.h
@@ -2,24 +2,41 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include"src/Common/Observer.h"
+#include"src/Common/BaseCommand.h"
+#include<memory>
+#include <QMediaPlayer>
+#include <QMediaPlaylist>
+#include<vector>
+#include<string>
+#include<QString>
+#include"src/Viewmodel/ViewModel.h"
 namespace Ui {
 class MainWindow;
 }
 
-class MainWindow : public QMainWindow
+class MainWindow : public QMainWindow, public Observer
 {
     Q_OBJECT
 
 public:
     explicit MainWindow(QWidget *parent = 0);
+    virtual void update(Notify noti);
+    void setplaylist(shared_ptr<QMediaPlaylist> playList1){playList=playList1;}
+    void setaddMusicToListCommand(shared_ptr<BaseCommand> add){addMusicToListCommand=add;}
+
     ~MainWindow();
 
 private slots:
-    void on_pushButton_clicked();
+    void on_PlayButton_clicked();
+
+    void on_AddMuiscButton_clicked();
 
 private:
     Ui::MainWindow *ui;
+    shared_ptr<QMediaPlayer> player;
+    shared_ptr<QMediaPlaylist>playList;
+    shared_ptr<BaseCommand> addMusicToListCommand,musicPlayCommand;
 };
 
 #endif // MAINWINDOW_H
