27abcd77a1fcebac75d0c5b1a08fa0272e927c20
linlinlinser <535087237@qq.com>
2017-07-14 23:49:00 +0800

    Update Model.cpp

--- src/Model/Model.cpp
+++ src/Model/Model.cpp
@@ -1,6 +1,8 @@
 #include"src/Model/Model.h"
 #include "src/Common/soundTouch/change.hpp"
 #include <QDebug>
+#include<QString>
+#include<QStringList>
 void Model::addPath(const vector<QString> path)
 {
     for (size_t i = 0; i < path.size(); i++)
@@ -18,9 +20,13 @@ void Model::changeFreq(const QString &path, const double &freq)
     qDebug() << "Model changing freq..." << endl;
     const QString tone = "A6";
     const string addr = path.toStdString();
+    qDebug()<<path;
     const string temp = tone.toStdString();
-    change_once(addr, temp, freq);
+    QString tem=change_once(addr, temp, freq);
     qDebug() << "Freq is changed" << endl;
+    vector<QString>tems;
+    tems.push_back(tem);
+    addPath(tems);
     Notify noti;
     noti.settype(Notifys::ChangeFreq);
     notify(noti);
